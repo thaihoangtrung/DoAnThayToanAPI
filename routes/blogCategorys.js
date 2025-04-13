73bb388b2cbd28e4fc8e67d797b9a2903cb0f423
trung <hoangtrungp0205@gmail.com>
2025-04-13 14:15:56 +0700

    chore rafctor code blog category

--- routes/blogCategorys.js
+++ routes/blogCategorys.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var blogcategoryController = require('../controllers/blogCategory')
 let {CreateErrorRes,CreateSuccessRes} = require('../utils/responseHandler')
 
-/* GET users listing. */
 router.get('/', async function(req, res, next) {
   let blogcategorys = await blogcategoryController.GetAllBlogCategorys();
   CreateSuccessRes(res,blogcategorys,200);
@@ -18,22 +17,18 @@ router.post('/', async function(req, res, next) {
  }
 });
 
-// Update Blog Category
 router.put('/:id', async function(req, res, next) {
     try {
         const { id } = req.params;
-        const updateData = req.body; // Contains name and/or description
+        const updateData = req.body;
 
-        // Basic validation: Check if updateData is provided and not empty
         if (!updateData || Object.keys(updateData).length === 0) {
-             // Use CreateErrorRes for sending client-side errors
              return CreateErrorRes(res, "Update data (name or description) is required", 400);
         }
 
         let updatedCategory = await blogcategoryController.UpdateBlogCategory(id, updateData);
         CreateSuccessRes(res, updatedCategory, 200);
     } catch (error) {
-        // Pass controller/database errors to the central error handler
         next(error);
     }
 });
